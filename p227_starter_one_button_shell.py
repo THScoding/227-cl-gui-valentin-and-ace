70abc65c98aa80b095a79e91dfc66f4c3681db20
AwesomeGuy17 <acebrodhunludke@gmail.com>
2026-01-14 09:53:23 -0800

    Beginning, added all PLTW stuff

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -1,22 +1,82 @@
+from fileinput import filename
 import subprocess
 import tkinter as tk
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command():
-    command = ["ping", "localhost"]
-    # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
-    # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
+def do_command(command):
+
+    global command_textbox, url_entry
     
-    subprocess.run(command)
-    
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        url_val = "127.0.0.1"
+        #url_val = "::1"
+
+    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            command_textbox.insert(tk.END,line)
+            command_textbox.update()
+            
+
+
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
+ping_btn = tk.Button(frame, text="ping", command=lambda: do_command("ping"))
 ping_btn.pack()
+
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    #command=lambda:do_command("ping"),
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
+
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
+    command=lambda:do_command("ping"),
+    compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    bg="white", activebackground="gray")
+ping_btn.pack() 
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
+
+def mSave():
+    filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+    if filename is None:
+        return
+    file = open (filename, mode = 'w')
+    text_to_save = command_textbox.get("1.0", tk.END)
+
+    file.write(text_to_save)
+    file.close()
+
+save_button = tk.Button(frame, text="Save Output", command=mSave)
+save_button.pack()
 
 root.mainloop()
